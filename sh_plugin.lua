e382d4387d87ea32c8e9f7b00858f46446392464
Крыжовник <vasyl.lebedev@yandex.ru>
2023-04-23 00:13:13 +0300

    Update sh_plugin.lua

--- sh_plugin.lua
+++ sh_plugin.lua
@@ -1,34 +1,33 @@
-PLUGIN.name = "Turn based combat"
-PLUGIN.author = "Крыжовник#4511"
-PLUGIN.desc = "Плагин вдохновленный схемой ReHost'а."
+PLUGIN.name = "Combat Mode"
+PLUGIN.description = "A plugin that allows players to enter and exit combat mode with a key press."
+PLUGIN.author = "Крыжовник"
 
-function GM:PlayerButtonDown(ply, button)
-    if button == KEY_H then
-        ply:SetNWBool("CombatMode", !ply:GetNWBool("CombatMode"))
-        if ply:GetNWBool("CombatMode") then
-            ply:SetMoveType(MOVETYPE_NONE)
-            timer.Simple(0.1, function()
-                ply:ChatPrint(ply:Nick() .. " вошел в боевой режим!")
-            end)
+-- Создаем функцию, которая вызывается при нажатии клавиши H
+function PLUGIN:KeyPress(ply, key)
+    -- Проверяем, что игрок нажал клавишу H
+    if (key == IN_RELOAD) then
+        -- Проверяем, находится ли игрок в режиме боя
+        if (ply:GetNWBool("inCombat")) then
+            -- Выходим из режима боя
+            ply:SetNWBool("inCombat", false)
+            -- Размораживаем игрока
+            ply:Freeze(false)
+            -- Отправляем сообщение в локальный чат
+            chat.AddText(ply, Color(255, 255, 255), " has exited combat mode.")
         else
-            ply:SetMoveType(MOVETYPE_WALK)
-            timer.Simple(0.1, function()
-                ply:ChatPrint(ply:Nick() .. " вышел из боевого режима!")
-            end)
+            -- Входим в режим боя
+            ply:SetNWBool("inCombat", true)
+            -- Замораживаем игрока
+            ply:Freeze(true)
+            -- Устанавливаем количество ходов равным 10
+            ply:SetNWInt("turns", 10)
+            -- Отправляем сообщение в локальный чат
+            chat.AddText(ply, Color(255, 255, 255), " has entered combat mode.")
         end
     end
 end
 
-hook.Add("OnContextMenuOpen", "AddTargetOption", function()
-    local menu = DermaMenu()
-    for _, ply in ipairs(player.GetAll()) do
-        if ply ~= LocalPlayer() then
-            menu:AddOption("Сделать целью", function()
-                net.Start("SetTarget")
-                net.WriteEntity(ply)
-                net.SendToServer()
-            end)
-        end
-    end
-    menu:Open()
+-- Создаем хук KeyPress, который вызывает нашу функцию при нажатии клавиши
+hook.Add("KeyPress", "CombatMode", function(ply, key)
+    PLUGIN:KeyPress(ply, key)
 end)
