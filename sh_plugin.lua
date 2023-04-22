6bc4997856b9ed77b92bdb7f0d925a79388db756
Крыжовник <vasyl.lebedev@yandex.ru>
2023-04-23 00:22:03 +0300

    Update sh_plugin.lua

--- sh_plugin.lua
+++ sh_plugin.lua
@@ -1,3 +1,7 @@
+PLUGIN.name = "Combat system like Rehost"
+PLUGIN.author = "Крыжовник#4511"
+PLUGIN.description = "Combat system from Cyberpunk Rehost."
+
 -- Создаем переменные для хранения состояния боевого режима и количества ходов
 local combatMode = false
 local combatTurn = false
@@ -70,4 +74,69 @@ function ToggleCombatTurn()
             -- Выводим сообщение в локальный чат и уведомление об оставшихся ходах 
             ix.chat.Send(LocalPlayer(), "me", "начинает свой ход.")
             ix.util.Notify("У вас "..combatMoves.." ходов.")
-            -- Р
+            -- Разрешаем игроку двигаться и атаковать
+            hook.Remove("StartCommand", "CombatModeRestrictions")
+            -- Создаем меню атак на клавишу "I"
+            hook.Add("PlayerButtonDown", "CombatAttackKey", function(client, key)
+                if key == KEY_I then
+                    -- Создаем панель меню
+                    local panel = vgui.Create("DFrame")
+                    panel:SetSize(300, 200)
+                    panel:Center()
+                    panel:SetTitle("Меню атак")
+                    panel:MakePopup()
+                    -- Создаем кнопку для обычной атаки
+                    local attackButton = vgui.Create("DButton", panel)
+                    attackButton:SetSize(100, 50)
+                    attackButton:SetPos(100, 50)
+                    attackButton:SetText("Атаковать")
+                    attackButton.DoClick = function()
+                        -- Проверяем, что у игрока есть ходы
+                        if combatMoves > 0 then
+                            -- Отнимаем один ход
+                            combatMoves = combatMoves - 1
+                            -- Выводим сообщение об атаке в локальный чат и уведомление об оставшихся ходах
+                            ix.chat.Send(LocalPlayer(), "me", "атакует "..LocalPlayer().combatTarget:Name()..".")
+                            ix.util.Notify("У вас "..combatMoves.." ходов.")
+                            -- Наносим урон цели в зависимости от оружия игрока
+                            local weapon = LocalPlayer():GetActiveWeapon()
+                            if weapon then
+                                local damage = weapon:GetDamage()
+                                LocalPlayer().combatTarget:TakeDamage(damage, LocalPlayer(), weapon)
+                            end
+                        else
+                            -- Иначе выводим сообщение об ошибке
+                            ix.util.Notify("У вас нет ходов.")
+                        end
+                        -- Закрываем меню атак
+                        panel:Close()
+                    end
+                end
+            end)
+        else
+            -- Если игрок закончил ход
+            -- Выводим сообщение в локальный чат и уведомление о конце хода 
+            ix.chat.Send(LocalPlayer(), "me", "заканчивает свой ход.")
+            ix.util.Notify("Ваш ход закончился.")
+            -- Запрещаем игроку двигаться и атаковать
+            hook.Add("StartCommand", "CombatModeRestrictions", function(client, cmd)
+                cmd:ClearMovement()
+                cmd:ClearButtons()
+            end)
+            -- Удаляем меню атак с клавиши "I"
+            hook.Remove("PlayerButtonDown", "CombatAttackKey")
+            -- Восстанавливаем количество ходов до стандартного значения
+            combatMoves = 10
+        end
+    else
+        -- Если игрок не находится в боевом режиме или не имеет цели для атаки, выводим сообщение об ошибке
+        ix.util.Notify("Вы не можете начать или закончить ход без боевого режима и цели.")
+    end
+end
+
+-- Привязываем функцию к клавише "J"
+hook.Add("PlayerButtonDown", "CombatTurnKey", function(client, key)
+    if key == KEY_J then
+        ToggleCombatTurn()
+    end
+end)
