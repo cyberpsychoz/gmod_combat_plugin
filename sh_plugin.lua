75b3b720ab5fcefc04a6ed6255288ca020908889
Крыжовник <vasyl.lebedev@yandex.ru>
2023-04-23 00:20:03 +0300

    GLOBAL sh_plugin.lua

--- sh_plugin.lua
+++ sh_plugin.lua
@@ -1,61 +1,73 @@
-PLUGIN.name = "Combat Mode"
-PLUGIN.description = "A plugin that allows players to enter and exit combat mode with a key press."
-PLUGIN.author = "Крыжовник"
+-- Создаем переменные для хранения состояния боевого режима и количества ходов
+local combatMode = false
+local combatTurn = false
+local combatMoves = 10
 
--- Создаем функцию, которая вызывается при нажатии клавиши H
-function PLUGIN:KeyPress(ply, key)
-    -- Проверяем, что игрок нажал клавишу H
-    if (key == IN_RELOAD) then
-        -- Проверяем, находится ли игрок в режиме боя
-        if (ply:GetNWBool("inCombat")) then
-            -- Выходим из режима боя
-            ply:SetNWBool("inCombat", false)
-            -- Размораживаем игрока
-            ply:Freeze(false)
-            -- Отправляем сообщение в локальный чат
-            chat.AddText(ply, Color(255, 255, 255), " has exited combat mode.")
+-- Создаем функцию для входа и выхода из боевого режима на клавишу "H"
+function ToggleCombatMode()
+    -- Проверяем, что игрок не находится в ходе
+    if not combatTurn then
+        -- Переключаем состояние боевого режима
+        combatMode = not combatMode
+        -- Если игрок вошел в боевой режим
+        if combatMode then
+            -- Выводим сообщение в локальный чат
+            ix.chat.Send(LocalPlayer(), "me", "входит в боевой режим.")
+            -- Запрещаем игроку двигаться и атаковать
+            hook.Add("StartCommand", "CombatModeRestrictions", function(client, cmd)
+                cmd:ClearMovement()
+                cmd:ClearButtons()
+            end)
+            -- Добавляем пункт Target в контекстное меню
+            hook.Add("PopulateContextMenu", "CombatModeTarget", function(panel)
+                panel:AddOption("Target", function()
+                    -- Получаем трассировку луча от курсора игрока
+                    local trace = LocalPlayer():GetEyeTrace()
+                    -- Если луч попал в другого игрока
+                    if trace.Entity:IsPlayer() then
+                        -- Задаем его как цель для атаки
+                        LocalPlayer().combatTarget = trace.Entity
+                        -- Выводим сообщение об этом
+                        ix.util.Notify("Вы выбрали "..trace.Entity:Name().." как цель.")
+                    else
+                        -- Иначе выводим сообщение об ошибке
+                        ix.util.Notify("Вы должны выбрать игрока как цель.")
+                    end
+                end)
+            end)
         else
-            -- Входим в режим боя
-            ply:SetNWBool("inCombat", true)
-            -- Замораживаем игрока
-            ply:Freeze(true)
-            -- Устанавливаем количество ходов равным 10
-            ply:SetNWInt("turns", 10)
-            -- Отправляем сообщение в локальный чат
-            chat.AddText(ply, Color(255, 255, 255), " has entered combat mode.")
+            -- Если игрок вышел из боевого режима
+            -- Выводим сообщение в локальный чат
+            ix.chat.Send(LocalPlayer(), "me", "выходит из боевого режима.")
+            -- Удаляем ограничения на движение и атаку
+            hook.Remove("StartCommand", "CombatModeRestrictions")
+            -- Удаляем пункт Target из контекстного меню
+            hook.Remove("PopulateContextMenu", "CombatModeTarget")
+            -- Сбрасываем цель для атаки
+            LocalPlayer().combatTarget = nil
         end
+    else
+        -- Если игрок находится в ходе, выводим сообщение об ошибке
+        ix.util.Notify("Вы не можете покинуть боевой режим во время хода.")
     end
 end
 
--- Создаем хук KeyPress, который вызывает нашу функцию при нажатии клавиши
-hook.Add("KeyPress", "CombatMode", function(ply, key)
-    PLUGIN:KeyPress(ply, key)
+-- Привязываем функцию к клавише "H"
+hook.Add("PlayerButtonDown", "CombatModeKey", function(client, key)
+    if key == KEY_H then
+        ToggleCombatMode()
+    end
 end)
 
--- Создаем опцию Target в контекстном меню
-properties.Add("Target", {
-    -- Устанавливаем текст и иконку опции
-    MenuLabel = "Target",
-    MenuIcon = "icon16/eye.png",
-
-    -- Создаем функцию проверки, которая возвращает true, если выбранный объект является игроком и находится в зоне видимости
-    Filter = function(self, ent, ply)
-        -- Проверяем, что объект является игроком
-        if (!IsValid(ent) or !ent:IsPlayer()) then return false end
-        -- Проверяем, что объект находится в зоне видимости
-        local trace = ply:GetEyeTrace()
-        if (trace.Entity != ent) then return false end
-        -- Возвращаем true
-        return true
-    end,
-
-    -- Создаем функцию действия, которая устанавливает выбранный объект как цель для атаки
-    Action = function(self, ent)
-        -- Получаем локального игрока
-        local ply = LocalPlayer()
-        -- Устанавливаем выбранный объект как цель
-        ply:SetNWEntity("target", ent)
-        -- Отправляем сообщение в локальный чат
-        chat.AddText(Color(255, 255, 255), "You have selected ", ent:Nick(), " as your target.")
-    end
-})
+-- Создаем функцию для начала и окончания хода на клавишу "J"
+function ToggleCombatTurn()
+    -- Проверяем, что игрок находится в боевом режиме и имеет цель для атаки
+    if combatMode and LocalPlayer().combatTarget then
+        -- Переключаем состояние хода
+        combatTurn = not combatTurn
+        -- Если игрок начал ход
+        if combatTurn then
+            -- Выводим сообщение в локальный чат и уведомление об оставшихся ходах 
+            ix.chat.Send(LocalPlayer(), "me", "начинает свой ход.")
+            ix.util.Notify("У вас "..combatMoves.." ходов.")
+            -- Р
